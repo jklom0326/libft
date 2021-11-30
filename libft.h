18abe3427e106cbb3054ceb93bd06ce67b652c51
jklom0326 <jklom0326@gmail.com>
2021-11-30 22:02:05 +0900

    add function in head

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: shan <shan@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/18 18:29:34 by shan              #+#    #+#             */
-/*   Updated: 2021/11/18 18:29:34 by shan             ###   ########.fr       */
+/*   Updated: 2021/11/30 22:01:17 by shan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,20 @@
 # include <stdlib.h>
 # include <limits.h>
 
-typedef struct s_list
+typedef struct	s_list
 {
     void    *content;
     struct s_list   *next;
 }               t_list;
 
-
+int			ft_isalpha(int c);
+int			ft_isdigit(int c);
+int			ft_isalnum(int c);
+int			ft_isascii(int c);
+int			ft_isprint(int c);
+size_t		ft_strlen(const char *s);
+void		*ft_memset(void*b, int c, size_t len);
+void		ft_bzero(const void *s, int c, size_t n);
 
 
 
