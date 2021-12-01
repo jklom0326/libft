5b79f21c60352d2f06d18fe252dda63b72853112
jklom0326 <jklom0326@gmail.com>
2021-12-02 01:42:14 +0900

    head and first func

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: shan <shan@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/18 18:29:34 by shan              #+#    #+#             */
-/*   Updated: 2021/11/30 22:01:17 by shan             ###   ########.fr       */
+/*   Updated: 2021/12/01 18:05:10 by shan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,19 @@ int			ft_isprint(int c);
 size_t		ft_strlen(const char *s);
 void		*ft_memset(void*b, int c, size_t len);
 void		ft_bzero(const void *s, int c, size_t n);
-
-
+void		ft_memcpy(void *dst, const void *src, size_t n);
+void		ft_memmove(void *dst, const void *src, size_t len);
+size_t		ft_strlcpy(char *dst, const char *src, size_t dstsize);
+size_t		ft_strlcat(char *dst, const char *src, size_t dstsize);
+int			ft_toupper(int c);
+int			ft_tolower(int c);
+char		ft_strchr(const char *s, int c);
+char		ft_strrchr(const char *s, int c);
+void		ft_memchr(const void *s, int c, size_t n);
+int			ft_memcmp(const void *s1, const void *s2, size_t n);
+char		*ft_strnstr(const char *haystack, const char *needle, size_t len);
+int			ft_atoi(const char *str);
+void		ft_calloc(size_t count, size_t size);
+char		*ft_strdup(const char *s1);
 
 #endif
